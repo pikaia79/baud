05013ae20e01dac37a7a44fdc44178a084bf90d8
liangqingming <liangqingming@jd.com>
2018-04-23 16:18:34 +0800

    Update ps start function.

--- ps/server/admin.go
+++ ps/server/admin.go
@@ -1,22 +1,43 @@
 package server
 
 import (
+	"io/ioutil"
 	"os"
+	"strconv"
 
 	"github.com/tiglabs/baud/proto/metapb"
 )
 
-func (s *Server) createPartition(p *metapb.Partition) {
+func (s *Server) createPartition(p metapb.Partition) {
 
 }
 
-func (s *Server) destroyPartition(id metapb.PartitionID) {
-	os.RemoveAll(getPartitionPath(id, s.DataPath))
-}
-
-func (s *Server) destroyAll() {
-	os.RemoveAll(s.DataPath)
-	if err := os.MkdirAll(s.DataPath, os.ModePerm); err != nil {
-		panic(err)
+func (s *Server) destroyExcludePartition(partitions []metapb.Partition) {
+	dir, err := ioutil.ReadDir(s.DataPath)
+	if err != nil {
+		return
 	}
+
+	for _, fi := range dir {
+		if fi.IsDir() {
+			if id, err := strconv.ParseUint(fi.Name(), 10, 64); err == nil {
+				delete := true
+				for _, p := range partitions {
+					if p.ID == id {
+						delete = false
+						break
+					}
+				}
+
+				if delete {
+					path, _ := getPartitionPath(id, s.DataPath, false)
+					os.RemoveAll(path)
+				}
+			}
+		}
+	}
+}
+
+func (s *Server) reset() {
+	os.RemoveAll(s.DataPath)
 }
