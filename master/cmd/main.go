71341a75bc46918b21b044bb0943a100549695d2
yangxuanjia <yangxuanjia@jd.com>
2018-06-05 17:21:23 +0800

    restore master
    Signed-off-by: yangxuanjia <yangxuanjia@jd.com>

---
+++ master/cmd/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/tiglabs/baudengine/master"
+	"github.com/tiglabs/baudengine/util/log"
+	"github.com/tiglabs/raft/logger"
+	_ "net/http/pprof"
+	"os"
+	"os/signal"
+	"runtime"
+	"sync"
+	"syscall"
+)
+
+var (
+	configFile = flag.String("c", "", "config file path")
+	mainWg     sync.WaitGroup
+)
+
+type IServer interface {
+	Start(cfg *master.Config) error
+	Shutdown()
+}
+
+func interceptSignal(s IServer) {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		signal := <-sigs
+		log.Info("master received signal[%v]", signal.String())
+
+		s.Shutdown()
+		mainWg.Done()
+		os.Exit(0)
+	}()
+}
+
+func main() {
+	fmt.Println("Hello, BaudEsngine Master!")
+	flag.Parse()
+	fmt.Printf("configfile=[%v]\n", *configFile)
+
+	//for multi-cpu scheduling
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	cfg := master.NewConfig(*configFile)
+
+	log.InitFileLog(cfg.LogCfg.LogPath, cfg.ModuleCfg.Name, cfg.LogCfg.Level)
+	logger.SetLogger(log.GetFileLogger().SetRaftLevel(cfg.LogCfg.RaftLevel))
+	log.Debug("log has been initialized")
+
+	server := master.NewServer()
+
+	mainWg.Add(1)
+	//install the signal handler
+	interceptSignal(server)
+
+	//start the server
+	err := server.Start(cfg)
+	if err != nil {
+		log.Fatal("Fatal: failed to start the Baud Master daemon - ", err)
+	}
+
+	log.Info("Baud Master is running!")
+	mainWg.Wait()
+	log.Info("Goodbye, Baud Master!")
+}
---
+++ master/raft_snapshot.go
@@ -0,0 +1,77 @@
+package master
+
+import (
+	"github.com/gogo/protobuf/proto"
+	"github.com/tiglabs/baudengine/proto/masterpb"
+	"github.com/tiglabs/baudengine/util/raftkvstore"
+	raftproto "github.com/tiglabs/raft/proto"
+	"io"
+)
+
+type RaftSnapshot struct {
+	snap       raftkvstore.Snapshot
+	applyIndex uint64
+	iter       raftkvstore.Iterator
+}
+
+func NewRaftSnapshot(snap raftkvstore.Snapshot, applyIndex uint64, beginKey, endKey []byte) *RaftSnapshot {
+	s := &RaftSnapshot{
+		snap:       snap,
+		applyIndex: applyIndex,
+		iter:       snap.NewIterator(beginKey, endKey),
+	}
+
+	return s
+}
+
+func (s *RaftSnapshot) Next() ([]byte, error) {
+	err := s.iter.Error()
+	if err != nil {
+		return nil, err
+	}
+
+	hasNext := s.iter.Next()
+	if !hasNext {
+		return nil, io.EOF
+	}
+	kvPair := &masterpb.RaftKvPair{
+		Key:        s.iter.Key(),
+		Value:      s.iter.Value(),
+		ApplyIndex: s.ApplyIndex(),
+	}
+
+	return proto.Marshal(kvPair)
+}
+
+func (s *RaftSnapshot) ApplyIndex() uint64 {
+	return s.applyIndex
+}
+
+func (s *RaftSnapshot) Close() {
+	s.iter.Release()
+	s.snap.Release()
+}
+
+type SnapshotKVIterator struct {
+	rawIter raftproto.SnapIterator
+}
+
+func NewSnapshotKVIterator(rawIter raftproto.SnapIterator) *SnapshotKVIterator {
+	return &SnapshotKVIterator{
+		rawIter: rawIter,
+	}
+}
+
+func (i *SnapshotKVIterator) Next() (kvPair *masterpb.RaftKvPair, err error) {
+	var data []byte
+	data, err = i.rawIter.Next()
+	if err != nil {
+		return
+	}
+	kvPair = &masterpb.RaftKvPair{}
+	err = proto.Unmarshal(data, kvPair)
+	if err != nil {
+		return nil, err
+	}
+	return kvPair, nil
+}
