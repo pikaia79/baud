27f7a505a41a33970dbadebb1e0333a117e73956
yangyang65 <yangyang65@jd.com>
2018-06-05 10:49:08 +0800

    remove directory master

--- master/cmd/scripts/manage_db_create.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-source ./common.sh
-
-curl -v -d "db_name=mydb2" $LEADER_ADDR"/manage/db/create"
--- master/cmd/scripts/manage_space_create.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-source common.sh
-
-curl -v -d "db_name=mydb1&space_name=myspace1&partition_key=abc&partition_func=myfunc&partition_num=3" $LEADER_ADDR"/manage/space/create"
